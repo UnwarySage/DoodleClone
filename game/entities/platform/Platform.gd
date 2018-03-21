a930edc0385f50b145b2b7780b65a4eb0de231b1
unwarysage <unwary@comcast.net>
2018-03-21 13:24:15 -0500

    simplify platform spawning by setting view to non-widescreen

--- game/entities/platform/Platform.gd
+++ game/entities/platform/Platform.gd
@@ -2,6 +2,5 @@ extends StaticBody2D
 
 
 func _on_VisibilityNotifier2D_screen_exited():
-	print("ack")
 	self.queue_free()
 	
--- game/entities/platform_spawner/PlatformSpawn.gd
+++ game/entities/platform_spawner/PlatformSpawn.gd
@@ -4,6 +4,8 @@ export (PackedScene) var platform
 export (float) var stat_spawn_delay
 export (int) var spawn_area_width
 export (int) var spawn_area_margin
+export (float) var spawn_platform_shift = 300
+
 
 func _ready():
 	self.global_position.x = 0
@@ -11,16 +13,23 @@ func _ready():
 	#should be a reference to screen size
 	curve.add_point(Vector2(1024-spawn_area_margin,0))
 	
-	spawn_platform()
+	randomize()
+	
+
 
 func spawn_platform():
+	var shift = Vector2(0,0)
+	shift.x = randf() * (spawn_area_width - (spawn_area_margin * 2))
+	shift.x += spawn_area_margin
 	var spawn = platform.instance()
-	spawn.global_position = $SpawnPoint.global_position
+	spawn.global_position = Vector2(shift.x,self.global_position.y)
+	
 	get_tree().get_root().add_child(spawn)
-	$SpawnPoint.offset = randf() * curve.get_baked_length()
 	$SpawnTimer.wait_time = stat_spawn_delay
+	$SpawnTimer.start()
 	
-	
+
+
 
 func _on_SpawnTimer_timeout():
 	spawn_platform()
